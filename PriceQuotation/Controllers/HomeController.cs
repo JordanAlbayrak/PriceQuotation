650b9438bc893f4442f5c8cb228f8448c2f676ea
Jordan Alba <jordan008@live.ca>
2021-08-31 15:32:45 -0400

    Add project files.

---
+++ PriceQuotation/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using PriceQuotation.Models;
+
+
+namespace FutureValue.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            ViewBag.FV = 0;
+            ViewBag.DA = 0;
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Index(PriceQuotationModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewBag.FV = model.CalculatePriceQuotation();
+                ViewBag.DA = model.CalculateDiscountAmount();
+            }
+            else
+            {
+                ViewBag.FV = 0;
+                ViewBag.DA = 0;
+            }
+            return View(model);
+        }
+
+    }
+}
+
+
+
---
+++ PriceQuotation/Models/PriceQuotationModel.cs
@@ -0,0 +1,41 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace PriceQuotation.Models
+{
+    public class PriceQuotationModel
+    {
+        [Required(ErrorMessage =
+        "Please enter a Subtotal.")]
+        [Range(1, double.PositiveInfinity, ErrorMessage =
+        "Subtotal amount must be more than 0.")]
+        public double? Subtotal { get; set; }
+
+        [Required(ErrorMessage =
+        "Please enter a Discount Percentage.")]
+        [Range(0.1, 100.0, ErrorMessage =
+        "Yearly interest rate must be between 0.1 and 100.")]
+        public double? DiscountPercent { get; set; }
+        public double? CalculatePriceQuotation()
+        {
+
+            double? DiscountAmount = Subtotal * (DiscountPercent * 0.01);
+
+            double? Total = Subtotal - DiscountAmount;
+
+            return Total;
+        }
+        public double? CalculateDiscountAmount()
+        {
+
+            double? DiscountAmount = Subtotal * (DiscountPercent * 0.01);
+
+            return DiscountAmount;
+        }
+
+
+    }
+}
